62fd832552429313797a4b45f7fb35e62666a3f4
Liu <liukuixfan@163.com>
2022-04-24 16:29:08 +0800

    测试提交

--- order/src/main/java/com/order/MainOrder.java
+++ order/src/main/java/com/order/MainOrder.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class MainOrder
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(MainOrder.class,args);
     }
 }
